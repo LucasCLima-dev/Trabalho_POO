f532fbe1ddd4c54b2765dabc5a73cd0fca9aa8b9
Lucas-CLima-dev <lucas.limanunes@hotmail.com>
2024-11-17 15:10:57 -0300

    fifth commit

--- src/server.js
+++ src/server.js
@@ -3,3 +3,7 @@ import express from "express"
 const app = express();
 app.use(express.json)
 app.listen(3333, () => console.log("Servidor rodado"))
+
+app.get("/login", (req,res) => {
+    res.send("Olá, Alan")
+})
